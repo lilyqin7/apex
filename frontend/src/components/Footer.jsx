a9b249caad55605217928fd548224eff0a548546
lilyqin7 <lilyqin527@gmail.com>
2025-06-07 20:21:36 -0700

    header and footer responsive

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,22 +3,22 @@ import slackIcon from '../assets/slack-icon.png';
 import instagramIcon from '../assets/instagram-icon.png';
 
 function Footer() {
-    return <div className="bg-[#FF0000] w-screen h-[222px] px-[56px] py-[32px] flex items-center justify-center text-white">
-        <div className="flex flex-row space-x-[86.8px]">
-            <img src={logo} alt="Apex Logo" className="h-[176px] w-[167.2px]" />
-            <div className="flex flex-row space-x-[70px]">
+    return <div className="bg-[#FF0000] w-screen px-[20px] py-[20px] lg:px-[56px] lg:py-[32px] flex items-center justify-between text-white">
+        <div className="flex flex-row justify-between w-full">
+            <img src={logo} alt="Apex Logo" className="h-[15vw] lg:h-[176px] lg:w-[167.2px]" />
+            <div className="flex flex-col space-y-[30px] lg:flex-row lg:space-x-[70px]">
                 <div className="flex flex-col space-y-[13px]">
                     <div className="text-[26px]">Contact Us</div>
                     <div className="text-[18px]">cmu.apex@gmail.com</div>
                 </div>
-                <div className="flex flex-col space-y-[13px]">
+                <div className="flex flex-row space-x-[13px] lg:flex-col lg:space-y-[13px]">
                     <div className="text-[26px]">Join our Slack!</div>
                     {/* REPLACE THE SLACK LINK WHEN IT EXPIRES!!! */}
                     <a href="https://join.slack.com/t/apexbuggy/shared_invite/zt-367fbv1ck-K53JHZifaUMcK~1ZAIPwZg" target="_blank" rel="noopener noreferrer">
                         <img src={slackIcon} alt="Slack Icon" className="h-[50px] w-[50px]"/>
                     </a>
                 </div>
-                <div className="flex flex-col space-y-[13px]">
+                <div className="flex flex-row space-x-[13px] lg:flex-col lg:space-y-[13px]">
                     <div className="text-[26px]">Follow us on Instagram!</div>
                     <a href="https://www.instagram.com/apexbuggy/" target="_blank" rel="noopener noreferrer">
                         <img src={instagramIcon} alt="Instagram Icon" className="h-[50px] w-[50px]"/>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
+import blackLogo from '../assets/logo-black.png';
 import searchIcon from '../assets/search-icon.png';
 
 function Header({topLayer, bottomLayer, pageTitle, headerHeight}) {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return <div className="relative w-full" style={headerHeight ? { height: headerHeight } : {}}>
         <img src={bottomLayer} alt="Bottom Layer" className="absolute top-0 left-0 w-full object-cover z-0 pointer-events-none" />
         <img src={topLayer} alt="Top Layer" className="absolute top-0 left-0 w-full object-cover z-0 pointer-events-none" />
@@ -11,7 +15,16 @@ function Header({topLayer, bottomLayer, pageTitle, headerHeight}) {
                 <Link to='/'>
                     <img src={logo} alt="Apex Logo" className="h-[75px] w-[75px]" />
                 </Link>
-                <div className="flex flex-row space-x-[30px] text-[18px]">
+                <button 
+                    className="lg:hidden flex flex-col justify-center items-center"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                    aria-label="Toggle menu"
+                >
+                    <span className="block w-8 h-1 bg-white mb-1 rounded"></span>
+                    <span className="block w-8 h-1 bg-white mb-1 rounded"></span>
+                    <span className="block w-8 h-1 bg-white rounded"></span>
+                </button>
+                <div className="hidden lg:flex flex-row space-x-[30px] text-[18px]">
                     <Link to='/meettheteam'>Meet the Team</Link>
                     <Link to='/meetthebuggies'>Meet the Buggies</Link>
                     <Link to='/getinvolved'>Ways to Get Involved</Link>
@@ -19,6 +32,26 @@ function Header({topLayer, bottomLayer, pageTitle, headerHeight}) {
                     {/* <img src={searchIcon} alt="Search Icon" className="h-[20px] w-[20px]" /> */}
                 </div>
             </div>
+            {menuOpen && (
+                <div className="fixed top-0 left-0 w-full h-screen z-50 flex justify-center items-center lg:hidden">
+                    <button 
+                        className="absolute top-6 right-6 text-4xl text-black"
+                        onClick={() => setMenuOpen(false)}
+                        aria-label="Close menu"
+                    >
+                        &times;
+                    </button>
+                    <div className="flex flex-col items-center justify-center gap-y-4 py-4 text-[18px] w-full h-full bg-[#F0E9DD]">
+                        <Link to='/' onClick={() => setMenuOpen(false)}>
+                            <img src={blackLogo} alt="Apex Logo" className="h-[75px] w-[75px]" />
+                        </Link>
+                        <Link to='/meettheteam' onClick={() => setMenuOpen(false)}>Meet the Team</Link>
+                        <Link to='/meetthebuggies' onClick={() => setMenuOpen(false)}>Meet the Buggies</Link>
+                        <Link to='/getinvolved' onClick={() => setMenuOpen(false)}>Ways to Get Involved</Link>
+                        <Link to='/events' onClick={() => setMenuOpen(false)}>Upcoming Events</Link>
+                    </div> 
+                </div>
+            )}
             <h1 className="text-center">{pageTitle}</h1>
         </div>
     </div>
